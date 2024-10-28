2879a992015f36cc21783aafb82c4156ea0628a6
Ineschalet <chalet@insa-toulouse.fr>
2024-10-28 09:38:49 +0100

    tp1

--- developpement/MNIST/mnist_webapp.py
+++ developpement/MNIST/mnist_webapp.py
@@ -2,18 +2,22 @@ import gradio as gr
 from PIL import Image
 import requests
 import io
+import numpy as np
 
 
 def recognize_digit(image):
-    # Convert to PIL Image necessary if using the API method
-    image = Image.fromarray(image.astype('uint8'))
-    img_binary = ...
-    ...
-    ...
-    return ....
-
+    # Check if `image` is a NumPy array or a PIL Image
+    if isinstance(image, np.ndarray):
+        # Convert NumPy array to PIL Image if necessary
+        image = Image.fromarray(image.astype('uint8'))
+    img_binary = io.BytesIO()
+    image.save(img_binary, format="PNG")
+    # Send request to the API
+    response = requests.post("http://127.0.0.1:5001/predict", data=img_binary.getvalue())
+    return response.json()["prediction"]
+ 
 if __name__=='__main__':
-
+ 
     gr.Interface(fn=recognize_digit, 
                 inputs="sketchpad", 
                 outputs='label',
